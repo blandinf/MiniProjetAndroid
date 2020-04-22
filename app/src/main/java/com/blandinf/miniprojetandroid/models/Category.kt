935c379e8a7fd41c9578a68b9ca0a59111afdc93
Florian Blandin <pro.blandinf@gmail.com>
2020-04-22 11:56:04 +0200

    create differents models needed

---
+++ app/src/main/java/com/blandinf/miniprojetandroid/models/Category.kt
@@ -0,0 +1,8 @@
+package com.blandinf.miniprojetandroid.models
+
+data class Category(
+    val name: String,
+    val description: String,
+    val query: String,
+    val url: String
+)
---
+++ app/src/main/java/com/blandinf/miniprojetandroid/models/Country.kt
@@ -0,0 +1,6 @@
+package com.blandinf.miniprojetandroid.models
+
+data class Country(
+    val name: String,
+    val language: String
+)
